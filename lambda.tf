6113cf54942b45bfd6bd63980df0ee9477c2cfea
Djair Silva <djavasilva@yahoo.com.br>
2024-09-08 20:45:18 -0300

    correção readme

--- lambda.tf
+++ lambda.tf
@@ -31,13 +31,34 @@ resource "aws_iam_role_policy_attachment" "lambda_ssm_policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMFullAccess"
 }
 
+// Adiciona uma política personalizada para permitir lambda:GetPolicy
+resource "aws_iam_policy" "custom_lambda_policy" {
+  name        = "CustomLambdaPolicy"
+  description = "Permissão para executar lambda:GetPolicy na função sites"
+  policy      = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect   = "Allow",
+        Action   = ["lambda:GetPolicy"],
+        Resource = "arn:aws:lambda:us-east-1:730335588602:function:${var.nome_do_projeto}"
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "attach_custom_lambda_policy" {
+  role       = aws_iam_role.iam_for_lambda.name
+  policy_arn = aws_iam_policy.custom_lambda_policy.arn
+}
+
 resource "aws_lambda_function" "test_lambda" {
   filename         = "seu-site.zip"
   function_name    = var.nome_do_projeto
   role             = aws_iam_role.iam_for_lambda.arn
   handler          = var.handler
   runtime          = var.runtime
-  timeout          = 30  # Configura o tempo de execução para 30 segundos
+  timeout          = 30  // Configura o tempo de execução para 30 segundos
   environment {
     variables = {
       BUCKET_NAME      = "bucket-teste124"
